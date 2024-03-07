ecb392843e3ea8618807520390227b3b53c577e1
Najmus Sahar <najmus.sahar77@gmail.com>
2024-03-07 15:49:48 +0530

    layout

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,32 @@
+﻿using BoutiqueManagement.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoutiqueManagement.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class ItemsController : Controller
+    {
+        private IItemService _itemService;
+
+
+        public ItemsController(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+        // GET: Items
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult GetData()
+        {
+            var result = _itemService.GetItems();
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
